ad9bfcb8a1dad52dbe8af72fb77baf708206ccde
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-04-13 17:34:21 -0600

    New tool initialization model
    New model that rather than using a function to batch create tools, this creates tools on a one-by-one basis for more flexibility.
    Other changes:
     - Action button CSS makes text smaller
     - Created right toolbar for right action toolbar
     - Created help popup tool in right toolbar

--- toolbar.js
+++ toolbar.js
@@ -1,104 +1,46 @@
 var actionButton, actionButtonDisabled,
-  toolFactory = new OO.ui.ToolFactory(),
-	toolGroupFactory = new OO.ui.ToolGroupFactory(),
-	toolbar = new OO.ui.Toolbar( toolFactory, toolGroupFactory, { actions: true } );
+    toolFactory = new OO.ui.ToolFactory(),
+    toolGroupFactory = new OO.ui.ToolGroupFactory(),
+    toolbar = new OO.ui.Toolbar( toolFactory, toolGroupFactory, { actions: true } );
+    rightBar = new OO.ui.Toolbar( toolFactory, toolGroupFactory );
 
-
-function createTool( toolbar, group, name, icon, title, init, onSelect ) {
-	var Tool = function () {
-		Tool.super.apply( this, arguments );
-		this.toggled = false;
-		if ( init ) {
-			init.call( this );
-		}
-	};
-
-	OO.inheritClass( Tool, OO.ui.Tool );
-
-	Tool.prototype.onSelect = function () {
-		if ( onSelect ) {
-			onSelect.call( this );
-		} else {
-			this.toggled = !this.toggled;
-			this.setActive( this.toggled );
-		}
-		toolbar.emit( 'updateState' );
-	};
-	Tool.prototype.onUpdateState = function () {};
-
-	Tool.static.name = name;
-	Tool.static.group = group;
-	Tool.static.icon = icon;
-	Tool.static.title = title;
-	return Tool;
+function helpMenu( toolGroup, config ) {
+    OO.ui.PopupTool.call( this, toolGroup, $.extend( { popup: {
+        padded: true,
+        label: 'Help',
+        head: true
+    } }, config ) );
+    this.popup.$body.append( '<a href="http://performance.wmflabs.org/wiki/Main_Page" target="_blank">Performance Dev Website</a><br><br>' );
 }
+OO.inheritClass( helpMenu, OO.ui.PopupTool );
+helpMenu.static.name = 'help';
+helpMenu.static.icon = 'help';
+helpMenu.static.title = 'Help';
+toolFactory.register( helpMenu );
 
-
-toolbar.setup( [
-	{
-		type: 'bar',
-		include: [ { group: 'barTools' } ]
-	},
-	{
-		type: 'list',
-		indicator: 'down',
-		label: 'List',
-		icon: 'picture',
-		include: [ { group: 'listTools' } ],
-		allowCollapse: [ 'listTool4', 'listTool5', 'listTool6' ]
-	},
-	{
-		type: 'list',
-		indicator: 'down',
-		label: 'Auto-disabling list',
-		icon: 'picture',
-		include: [ { group: 'autoDisableListTools' } ]
-	},
-	{
-		type: 'menu',
-		indicator: 'down',
-		icon: 'alert',
-		include: [ { group: 'menuTools' } ]
-	},
-	{
-		type: 'list',
-		icon: 'picture',
-		include: [ { group: 'listTools' } ]
-	},
-	{
-    label: 'Catch-all',
-		include: '*'
-		}
+rightBar.setup( [
+    {
+        type: 'bar',
+        include: [ 'help' ]
+    },
 ] );
+
+toolFactory.register( helpMenu );
 
 actionButton = new OO.ui.ButtonWidget( { label: 'Action', flags: [ 'primary', 'progressive' ] } );
 actionButtonDisabled = new OO.ui.ButtonWidget( { label: 'Disabled', disabled: true } );
-toolbar.$actions.append( actionButton.$element, actionButtonDisabled.$element );
-
-toolbar.emit( 'updateState' );
-
-toolFactory.register( createTool( toolbar, 'barTools', 'barTool', 'picture', 'This is a test tool' ) );
-toolFactory.register( createTool( toolbar, 'barTools', 'disabledBarTool', 'picture', 'Basic tool in bar disabled', function () { this.setDisabled( true ); } ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool', 'picture', 'First basic tool in list' ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool1', 'picture', 'Basic tool in list' ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool2', 'picture', 'Another basic tool' ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool3', 'picture', 'Basic disabled tool in list', function () { this.setDisabled( true ); } ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool4', 'picture', 'More basic tools' ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool5', 'picture', 'And even more' ) );
-toolFactory.register( createTool( toolbar, 'listTools', 'listTool6', 'picture', 'A final tool' ) );
-
-toolFactory.register( createTool( toolbar, 'autoDisableListTools', 'autoDisableListTool', 'picture', 'Click to disable this tool', null, function () { this.setDisabled( true ); } ) );
-
-toolFactory.register( createTool( toolbar, 'menuTools', 'menuTool', 'check', 'Menu tool' ) );
-toolFactory.register( createTool( toolbar, 'menuTools', 'menuTool2', 'tag', 'Another menu tool' ) );
-
-toolFactory.register( createTool( toolbar, 'unusedStuff', 'unusedTool', 'help', 'This tool is not explicitly used anywhere' ) );
-toolFactory.register( createTool( toolbar, 'unusedStuff', 'unusedTool1', 'help', 'And neither is this one' ) );
-/*
-var tool = new OO.ui.Tool( 'barTools', {
-  name: 'barTool',
-  icon: 'picture',
-  title: 'This is a test tool'
-} ); */
+toolbar.$actions
+    .append(
+		  	$( '<div>' )
+		  	    .addClass( 'rightBar' )
+				    .append( rightBar.$element ),
+		  	$( '<div>' )
+		  	    .addClass( 'actionButtons' )
+				    .append( actionButton.$element, actionButtonDisabled.$element )
+);
+rightBar.emit( 'updateState' );
 
 $( document.body ).append( toolbar.$element );
+rightBar.initialize();
+toolbar.initialize();
+toolbar.emit( 'updateState' );
