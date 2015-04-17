0f08b2d83d1c1bf837fed2d4a340af9dabb7632f
Josh Dorsey <happyhillsbutchery@gmail.com>
2015-04-17 10:39:32 -0600

    - Moved to a class for action management
    though this only expands on the original in the sense that you can retrieve information on previous actions, as they don't seem to update in real time.

--- actions.js
+++ actions.js
@@ -1,19 +1,47 @@
 var name, title, type;
-if ( !(type === 'constructive' || type === 'destructive' || type === 'progressive') ) {
+if (!(type === 'constructive' || type === 'destructive' || type === 'progressive')) {
     type = null;
 }
 
-function createAction( name, title, type ) {
-    var newAction = new OO.ui.ButtonWidget( {
-        name: name,
-        label: title,
-        flags: [ type ],
-        //framed: false,
-    } );
-    toolbar.addItems( [ newAction ] );
-    toolbar.emit( 'updateState' );
+function ActionManager(t){
+	this.toolbar = t;
+	this.actions = [];
+	
+	this.updateActions = function(){
+		this.toolbar.items = [];
+		this.toolbar.addItems(this.actions);
+		this.toolbar.emit('updateState');
+	}
+
+	this.createAction = function (name, label, type){
+		var newAction = new OO.ui.ButtonWidget({
+			name: name,
+			label: label,
+			flags: [type],
+			// framed: false
+		});
+		this.actions.push(newAction);
+		this.updateActions();
+		return this.actions.length-1;
+	}
+	
+	this.changeAction = function(handle, name, label, type){
+		console.log("Retrieved Action");
+		console.dir(this.actions[handle]);
+		if(name != ""){
+			this.actions[handle].name = name;
+		}
+		if(label != ""){
+			this.actions[handle].label = label;
+		}
+		if(type != ""){
+			this.actions[handle].type = type;
+		}
+		this.updateActions();
+	}
 }
 
-createAction( 'test1', 'Test #1', 'constructive' );
-createAction( 'test2', 'Test #2', 'destructive' );
-createAction( 'test3', 'Test #3', 'progressive' );
+var am = new ActionManager(toolbar);
+var test1 = am.createAction('test1', 'Test #1', 'constructive');
+var test2 = am.createAction('test2', 'Test #2', 'destructive');
+var test3 = am.createAction('test3', 'Test #3', 'progressive');
